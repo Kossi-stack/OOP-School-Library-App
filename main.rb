7405022423fbd5e00af61934d7f02a211844a69c
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-11-19 14:18:56 -0800

    Create App class

--- main.rb
+++ main.rb
@@ -4,3 +4,16 @@ require './teacher'
 require './classroom'
 require './book'
 require './rental'
+class App
+    def initialize
+      @people = []
+      @books = []
+      @rentals = []
+      @class = Classroom.new('Grade 5')
+    end
+  
+    def run
+      print 'Welcome To School Library App'
+      sleep 0.75
+      menu
+    end
